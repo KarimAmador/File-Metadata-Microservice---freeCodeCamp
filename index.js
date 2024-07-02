03a493ed0d785d7d594dbd9d10ef3ce703d99886
KarimAmador <karimalejandroAC@gmail.com>
2024-07-02 09:39:09 -0600

    feat: add file post method to /fileanalyse route

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  console.log(req.file);
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
